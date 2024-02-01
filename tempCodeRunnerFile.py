1a9848e6895e4e02f8556608bd40af23d9ae7e33
Kinsey Cade <71044506+Cadek3@users.noreply.github.com>
2024-01-31 19:01:10 -0800

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-print("\nStatistics:")
-for func_name, stats in statistics.items():
-    print(f"{func_name}:")
-    for stat, value in stats.items():
-        print(f"{stat}: {value}")
-    print()
